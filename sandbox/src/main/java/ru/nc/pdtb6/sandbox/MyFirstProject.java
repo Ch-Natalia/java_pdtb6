3618d231aef57b55018664b77146b870dae5d3ee
Ch-Natalia <52357658+Ch-Natalia@users.noreply.github.com>
2019-08-21 00:43:11 +0300

    change name

---
+++ sandbox/src/main/java/ru/nc/pdtb6/sandbox/MyFirstProject.java
@@ -0,0 +1,8 @@
+package ru.nc.pdtb6.sandbox;
+
+public class MyFirstProject {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
--- sandbox/src/main/java/ru/nc/pdtb6/sandbox/myfirstproject.java
+++
@@ -1,9 +0,0 @@
-package ru.nc.pdtb6.sandbox;
-
-public class myfirstproject {
-
-	public static void main(String[] args){
- 		System.out.println("Hello, World!");
-}
-
-}
