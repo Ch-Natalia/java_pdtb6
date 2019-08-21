77ef54777f12b4d270d1cd3ee65dab52aa8aa1e4
Ch-Natalia <52357658+Ch-Natalia@users.noreply.github.com>
2019-08-21 21:40:11 +0300

    triangle

--- sandbox/src/main/java/ru/nc/pdtb6/sandbox/MyFirstProject.java
+++ sandbox/src/main/java/ru/nc/pdtb6/sandbox/MyFirstProject.java
@@ -2,7 +2,24 @@ package ru.nc.pdtb6.sandbox;
 
 public class MyFirstProject {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World");
+        Point a = new Point();
+        Point b = new Point();
+        a.x = 2;
+        a.y = 3;
+        b.x = 4;
+        b.y = 12;
+        int k1 = a.x - b.x;
+        if (k1 < 0){
+            k1 = k1 * (-1);
+        }
+        int k2 = a.y - b.y;
+        if (k2 < 0){
+            k2 = k2 * (-1);
+        }
+        int D = (k1*k1)+(k2*k2);
+        double d= Math.sqrt(D);
+        System.out.println(d);
     }
 
-}
+    }
---
+++ sandbox/src/main/java/ru/nc/pdtb6/sandbox/Point.java
@@ -0,0 +1,7 @@
+package ru.nc.pdtb6.sandbox;
+
+public class Point {
+
+    int x;
+    int y;
+}
