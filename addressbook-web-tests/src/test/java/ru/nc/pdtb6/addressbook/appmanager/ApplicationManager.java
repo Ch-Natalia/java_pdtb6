87ed1dbba67ba121ce99fa20cb014fc4bb212665
Ch-Natalia <52357658+Ch-Natalia@users.noreply.github.com>
2019-08-25 17:49:43 +0300

    2 browsers

--- addressbook-web-tests/src/test/java/ru/nc/pdtb6/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/nc/pdtb6/addressbook/appmanager/ApplicationManager.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,12 +17,26 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\soft\\chromedriver_win32_76\\chromedriver.exe");
-        wd = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver",
+                "C:\\soft\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+        if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.manage().deleteAllCookies();
         wd.get("http://localhost/addressbook");
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
--- addressbook-web-tests/src/test/java/ru/nc/pdtb6/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/nc/pdtb6/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.nc.pdtb6.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.nc.pdtb6.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
